dcc50e87f1cee792756debff304c7946c79546d6
User <avramenoo@mail.ru>
2021-04-05 22:42:31 +0300

    22:42

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem, QTableWidget
 from PyQt5.QtGui import QIcon
 from ui import Ui_MainWindow
 import MySQLdb as mdb
@@ -9,46 +9,58 @@ import MySQLdb as mdb
 class Program(QtWidgets.QMainWindow):
     def __init__(self):
         super(Program, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.window = Ui_MainWindow()
+        self.window.setupUi(self)
+
+        self.table = Table(self.window)
+
         self.db = None
-        self.cursor = None
-        self.connect_base()
-        self.print_table()
+        # self.connect_base()
+
+        tabs = ["Сотрудники", "Должности", "Отделы", "Имущество", "Поставщики", "Расположения", "Типы имущества"]
+        for tab in tabs:
+            temp = QtWidgets.QWidget()
+            temp.setGeometry(QtCore.QRect(0, 0, 0, 0))
+            self.window.tabWidget.addTab(temp, tab)
 
     def connect_base(self):
         self.db = mdb.connect('localhost', 'root', '321Ilyxazc', 'trpo_db')
-        # try:
-        #     db = mdb.connect('localhost', 'root', '321Ilyxazc', 'trpo_db')
-        #     QMessageBox.about(self, 'Connection', 'Database Connected Successfully')
-        #
-        # except mdb.Error as e:
-        #     QMessageBox.about(self, 'Connection', 'Failed To Connect Database')
-        #     sys.exit(1)
-
-    def print_table(self):
-        self.db = mdb.connect('localhost', 'root', '321Ilyxazc', 'trpo_db')
         self.db.set_character_set("utf8")
 
-        cursor = self.db.cursor()
 
-        cursor.execute("SELECT * FROM Должности")
+class Table:
+    def __init__(self, window):
+        self.table = window.tableWidget
+
+    def display_table(self, db, table_name):
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM {}".format(table_name))
 
         table = cursor.fetchall()
 
-        print(table)
-        self.ui.tableWidget.setRowCount(0)
-        self.ui.tableWidget.setColumnCount(0)
+        self.table.tableWidget.setRowCount(0)
+        self.table.tableWidget.setColumnCount(0)
 
         for row_number, row_data in enumerate(table):
-            self.ui.tableWidget.insertRow(row_number)
+            self.table.tableWidget.insertRow(row_number)
             for column_number, data in enumerate(row_data):
-                if self.ui.tableWidget.columnCount() <= column_number:
-                    self.ui.tableWidget.setColumnCount(self.ui.tableWidget.columnCount() + 1)
-                self.ui.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+                if self.table.tableWidget.columnCount() <= column_number:
+                    self.table.tableWidget.setColumnCount(self.table.tableWidget.columnCount() + 1)
+                self.table.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+# QTabBar::tab:selected  {
+#     background-color:rgb(255, 255, 0);
+# }
 
 
+# qss = '''
+# QTabBar::tab {
+#     background-color:rgb(220, 220, 0);
+# }
+# '''
 app = QtWidgets.QApplication([])
+# app.setStyleSheet(qss)
 application = Program()
 application.show()
 
